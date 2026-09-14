531fde8c6e2e6a0c50af85fc840bde00f4ba6bbf
tebasaki-bsg <sr71btblackbird@gmail.com>
2026-09-15 05:12:58 +0900

    first commit

---
+++ src/StabAndTarget_assem/Mod.cs
@@ -0,0 +1,14 @@
+using System;
+using Modding;
+using UnityEngine;
+
+namespace StaTSpace
+{
+	public class Mod : ModEntryPoint
+	{
+		public override void OnLoad()
+		{
+			// Called when the mod is loaded.
+		}
+	}
+}
